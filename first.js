9f02087bc9d7dec6d994e00e66b8ac37944804f5
Roqee <97388992+Roqee@users.noreply.github.com>
2022-01-09 20:37:05 +0800

    创建first.js文件
    第一次使用GitHub创建项目文件

---
+++ first.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>复杂一些的JSON对象。</title>
+	</head>
+	<body>
+		<script>
+			window.eval("var i = 100");
+			// alert(i);
+			// var reg = /(?=(\d{3})+$)/g;
+			// var reg1 = /(?=(\B)(\d{3})+$)/g;
+			var reg = /(?=(\B)(\d{3})+$)/g;
+            // /(?=(\B)(\d{3})+$)/
+			var str = "100000000000";
+			var str1 = str.replace(reg, ".");
+			console.log(str1);
+		</script>
+	</body>
+</html>
